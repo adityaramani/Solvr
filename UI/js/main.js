3de3540e08bda62474c4aab278596bd5880d1bc0
vidya venkappa <adityaramani@hotmail.com>
2018-11-12 18:16:30 +0530

    UI simple eq form

--- UI/js/main.js
+++ UI/js/main.js
@@ -89,7 +89,7 @@ $(document).on('ready', function () {
 		loop: true,
 		grabCursor: true,
 		centeredSlides: true,
-		autoplay: 5000,
+		autoplay: 0,
 		autoplayDisableOnInteraction: false,
 		slidesPerView: 1,
 		spaceBetween: 0,
@@ -284,5 +284,123 @@ $(document).on('ready', function () {
 		$('.section').addClass('anim');
 	});
 
+
+});
+
+
+
+var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}} // ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+var
+TextScramble = function () {
+  function TextScramble(el) {_classCallCheck(this, TextScramble);
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}—=+*^?#________';
+    this.update = this.update.bind(this);
+  }_createClass(TextScramble, [{ key: 'setText', value: function setText(
+    newText) {var _this = this;
+      var oldText = this.el.innerText;
+      var length = Math.max(oldText.length, newText.length);
+      var promise = new Promise(function (resolve) {return _this.resolve = resolve;});
+      this.queue = [];
+      for (var i = 0; i < length; i++) {
+        var from = oldText[i] || '';
+        var to = newText[i] || '';
+        var start = Math.floor(Math.random() * 40);
+        var end = start + Math.floor(Math.random() * 40);
+        this.queue.push({ from: from, to: to, start: start, end: end });
+      }
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    } }, { key: 'update', value: function update()
+    {
+      var output = '';
+      var complete = 0;
+      for (var i = 0, n = this.queue.length; i < n; i++) {var _queue$i =
+        this.queue[i],from = _queue$i.from,to = _queue$i.to,start = _queue$i.start,end = _queue$i.end,char = _queue$i.char;
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += '<span class="dud">' + char + '</span>';
+        } else {
+          output += from;
+        }
+      }
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
+      }
+    } }, { key: 'randomChar', value: function randomChar()
+    {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+    } }]);return TextScramble;}();
+
+
+
+
+
+var app = angular.module('SolvrApp', []);
+app.controller('formCtrl', function($scope) {
+	$scope.rhs = null;
+	$scope.x_coeff= null;
+
+    $scope.solve = function(e) {
+
+        $(".ripple").remove();
+        var cnt = $(".input-equation")[0];
+		var posX = $(cnt).offset().left,
+		posY = $(cnt).offset().top,
+		buttonWidth = $(cnt).width(),
+		buttonHeight =  $(cnt).height();
+
+  	// Add the element
+  		$(cnt).prepend("<span class='ripple'></span>");
+
+  
+ // Make it round!
+		if(buttonWidth >= buttonHeight) {
+    		buttonHeight = buttonWidth;
+  		} else {
+    		buttonWidth = buttonHeight; 
+ 		 }
+  
+  // Get the center of the element
+  		var x = e.pageX - posX - buttonWidth / 2;
+  		var y = e.pageY - posY - buttonHeight / 2;
+  
+ 
+  // Add the ripples CSS and start the animation
+		$(".ripple").css({
+		    width: buttonWidth,
+		    height: buttonHeight,
+		    top: y + 'px',
+		    left: x + 'px'
+		}).addClass("rippleEffect");
+	
+		var text = document.createElement("h3");
+		text.className = "text-scrambler";
+		text.innerHTML= $scope.x_coeff + "X = " + $scope.rhs; 
+		
+		setTimeout(function() { 
+			$("#singleVarEq").remove();
+			$(".input-equation").append(text);
+		 	var el = document.querySelector('.text-scrambler');
+			var fx = new TextScramble(el);
+			fx.setText("x = 4")
+		 }, 550);
+
+		
+	};
 
 });
